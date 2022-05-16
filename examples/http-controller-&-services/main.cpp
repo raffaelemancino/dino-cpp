b5c25763ee64092566db84cf194ed930e556e14a
Raffaele Mancino <raffaelefrancesco.mancino@gmail.com>
2022-05-16 14:36:14 +0000

    license add

--- examples/http-controller-&-services/main.cpp
+++ examples/http-controller-&-services/main.cpp
@@ -5,10 +5,16 @@
 
 #include "dino/recursion/ddfs.hpp"
 
+#include "app.module.hpp"
+
 using namespace std;
 
 int main()
 {
+    // Dino::App *application = Dino::App::getInstance();
+    // application->bootstrap<AppModule>();
+    // application->bind(8080, "api");
+
     Graph *n = new Graph();
     n->addElement("A", "B");
     n->addElement("A", "D");
